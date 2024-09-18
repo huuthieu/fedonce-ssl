2c6d1ad0310d4292e7ca3484bea1d907b3622035
huuthieu <huuthieucqt@gmail.com>
2024-09-18 11:32:52 +0700

    Update

---
+++ run_phishing_custom.sh
@@ -0,0 +1,8 @@
+python run_phishing_custom.py --number 0 > log/phishing/party_0/fedonce_ft_dae_selection_guest_rd10.txt
+rm cache/*
+python run_phishing_custom.py --number 1 > log/phishing/party_0/fedonce_ft_dae_selection_guest_rd10_reps05.txt
+rm cache/*
+python run_phishing_custom.py --number 2 > log/phishing/party_0/fedonce_ft_dae_selection_guest_rd15_reps15.txt
+rm cache/*
+# python run_phishing_custom.py --number 3 > log/phishing/party_0/fedonce_ft_dae_selection_guest_rd10_reps15.txt
+# rm cache/*
